b71cd9dc521059cfcd8498ddaf4dbfd9aee33dfe
Jason Prikken <jasonprikken00@gmail.com>
2021-10-11 14:12:11 +0200

    Connected LoginViewController to MainViewController

--- src/main/java/com/nsimate/nsimate/LoginViewController.java
+++ src/main/java/com/nsimate/nsimate/LoginViewController.java
@@ -1,9 +1,11 @@
 package com.nsimate.nsimate;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.stage.Stage;
@@ -14,16 +16,19 @@ import java.io.IOException;
 public class LoginViewController {
     public TextField userNameField;
     public TextField passwordField;
+    public Button loginButton;
+    public Button cancelButton;
+
     String username, password;
 
-    @FXML
     public void onLoginButton() {
         username = userNameField.getText();
         password = passwordField.getText();
 
         if(DatabaseConnect.databaseConnect(username, password)) {
-
             try {
+                Stage stage = (Stage) loginButton.getScene().getWindow();
+                stage.close();
                 MainViewController.start(new Stage());
             } catch(Exception e) {
                 e.printStackTrace();
@@ -32,6 +37,7 @@ public class LoginViewController {
     }
 
     public void onCancelButton() {
-
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
     }
 }
